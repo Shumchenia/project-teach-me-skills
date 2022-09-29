3d79f5ac05b289d18495c997c2b81b27f5182961
Matvei Shumchenia <shumchenia.mat@gmail.com>
2022-09-29 14:12:41 +0300

    add new project

---
+++ src/test/java/com/example/projectteachmeskills/ProjectTeachMeSkillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectteachmeskills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTeachMeSkillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
